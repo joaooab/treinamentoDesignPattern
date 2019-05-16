30174e398681d27ec999a6ecf9e4a576dc67aaaf
joao.freitas <joao.freitas@maximasistemas.com.br>
2019-05-16 18:14:44 -0300

    refatoração strategy test

--- src/strategy/CalculadorDeImpostoTest.java
+++ src/strategy/CalculadorDeImpostoTest.java
@@ -7,10 +7,18 @@ import static org.junit.Assert.assertEquals;
 public class CalculadorDeImpostoTest {
 
     @Test
-    public void calculoImposto() {
+    public void calculaIcms() {
         final Orcamento orcamento = new Orcamento(20d);
         Double resultado = new CalculadorDeImposto().realizaCalculo(orcamento, "ICMS");
         Double resultadoEsperado = 2d;
+        assertEquals(resultado, resultadoEsperado);
+    }
+
+    @Test
+    public void calculaIss() {
+        final Orcamento orcamento = new Orcamento(50d);
+        Double resultado = new CalculadorDeImposto().realizaCalculo(orcamento, "ISS");
+        Double resultadoEsperado = 3d;
         assertEquals(resultado, resultadoEsperado);
     }
 
