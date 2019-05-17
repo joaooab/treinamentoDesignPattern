889acf67cb656fb3c19c901d089eaf6777916c33
joaoab <joaoovfreitas@gmail.com>
2019-05-16 21:44:56 -0300

    create factory refatorado
    (cherry picked from commit 8bbfd628116566b6d7f843a00f1ab934962b4efc)

--- src/factory/Programa.java
+++ src/factory/Programa.java
@@ -1,16 +1,17 @@
 package factory;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 class Programa {
-
-    public static void main(String[] args) throws SQLException {
-        Connection c = ConnectionFactory.getConnection();
-        if (c != null) {
+    public static void main(String[] args)  {
+        try {
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost/banco", "root" , "1234");
             PreparedStatement ps = c.prepareStatement("select * from tabela");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-
 }
