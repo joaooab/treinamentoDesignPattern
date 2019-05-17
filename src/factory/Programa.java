6f8794f494efe5252608896cae87ee3d558e71e6
joaoab <joaoovfreitas@gmail.com>
2019-05-16 22:13:12 -0300

    Revert "create factory refatorado"
    This reverts commit 889acf67

--- src/factory/Programa.java
+++ src/factory/Programa.java
@@ -5,12 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 class Programa {
-
     public static void main(String[] args) throws SQLException {
         Connection c = ConnectionFactory.getConnection();
         if (c != null) {
             PreparedStatement ps = c.prepareStatement("select * from tabela");
         }
     }
-
 }
