f96de1f9f10adf2e704aa19e70a36ccdd2d756c2
joaoab <joaoovfreitas@gmail.com>
2019-05-16 21:46:10 -0300

    Revert "create factory refatorado"
    This reverts commit 889acf67

--- src/factory/Programa.java
+++ src/factory/Programa.java
@@ -1,17 +1,16 @@
 package factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 class Programa {
-    public static void main(String[] args)  {
-        try {
-            Connection c = DriverManager.getConnection("jdbc:mysql://localhost/banco", "root" , "1234");
+
+    public static void main(String[] args) throws SQLException {
+        Connection c = ConnectionFactory.getConnection();
+        if (c != null) {
             PreparedStatement ps = c.prepareStatement("select * from tabela");
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
+
 }
