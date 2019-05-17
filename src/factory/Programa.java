30eb8a0fa52c1e38875fc241042a09373eafb3c4
joaoab <joaoovfreitas@gmail.com>
2019-05-16 21:33:56 -0300

    create factory

---
+++ src/factory/Programa.java
@@ -0,0 +1,15 @@
+package factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+class Programa {
+
+    public static void main(String[] args) throws SQLException {
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/banco", "root" ,"1234");
+        PreparedStatement ps = c.prepareStatement("select * from tabela");
+    }
+
+}
