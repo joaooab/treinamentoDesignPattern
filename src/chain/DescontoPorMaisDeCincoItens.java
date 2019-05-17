ceaa3081c095c7f304cd6a6420a486b0067a4324
joao.freitas <joao.freitas@maximasistemas.com.br>
2019-05-17 13:46:36 -0300

    create chain - refatorado

--- src/chain/DescontoPorMaisDeCincoItens.java
+++ src/chain/DescontoPorMaisDeCincoItens.java
@@ -1,16 +1,16 @@
 package chain;
 
 public class DescontoPorMaisDeCincoItens implements Desconto {
-	private Desconto proximo;
+    private Desconto proximo;
 
-	@Override
-	public Double desconta(Orcamento orcamento) {
-		if (orcamento.getItens().size() > 5) {
-			return orcamento.getValor() * 0.1;
-		} else {
-			return proximo.desconta(orcamento);
-		}
-	}
+    @Override
+    public Double desconta(Orcamento orcamento) {
+        if (orcamento.getItens().size() > 5) {
+            return orcamento.getValor() * 0.1;
+        } else {
+            return proximo.desconta(orcamento);
+        }
+    }
 
     @Override
     public void setProximo(Desconto proximo) {
