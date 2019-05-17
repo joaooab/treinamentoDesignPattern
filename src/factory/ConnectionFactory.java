1d82c14ffadec7389b696a7cadfcf1b756494d17
joaoab <joaoovfreitas@gmail.com>
2019-05-16 21:40:14 -0300

    create factory refatorado

---
+++ src/factory/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+class ConnectionFactory {
+
+    static Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost/banco", "root" , "1234");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/factory/Programa.java
+++ src/factory/Programa.java
@@ -1,15 +1,16 @@
 package factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 class Programa {
 
     public static void main(String[] args) throws SQLException {
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/banco", "root" ,"1234");
-        PreparedStatement ps = c.prepareStatement("select * from tabela");
+        Connection c = ConnectionFactory.getConnection();
+        if (c != null) {
+            PreparedStatement ps = c.prepareStatement("select * from tabela");
+        }
     }
 
 }
